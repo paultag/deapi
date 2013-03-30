955ab991d0269b9ae80849789494490b39dfd6c2
Nathan Handler <nhandler@debian.org>
2013-03-30 15:27:21 -0500

    s/pts/bts/ typo and combine most of the BTS query methods into one generic query

--- deapi/blueprints/bts.py
+++ deapi/blueprints/bts.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, abort, request
 from deapi.emitters import emit
-from deapi.services.pts import query
+from deapi.services.bts import query, query_get_versions
 
 
 bts = Blueprint(
@@ -12,26 +12,27 @@ bts = Blueprint(
 
 @bts.route("/get-status/<bugnumbers>", methods=['GET'])
 def get_status(bugnumbers):
-    return emit(query_get_status(bugnumbers), True)
+    return emit(query("get_status", bugnumbers), True)
 
 
 @bts.route("/get-bugs/<conditions>", methods=['GET'])
 def get_bugs(conditions):
-	return emit(query_get_bugs(conditions), True)
+	return emit(query("get_bugs", conditions), True)
+
 
 @bts.route("/get-usertag/<email>", methods=['GET'])
 def get_usertag(email):
-	return emit(query_get_usertag(email), True)
+	return emit(query("get_usertag", email), True)
 
 
 @bts.route("/get-bug-log/<int:bugnumber>", methods=['GET'])
 def get_bug_log(bugnumber):
-	return emit(query_get_bug_log(bugnumber), True)
+	return emit(query("get_bug_log", bugnumber), True)
 
 
 @bts.route("/newest-bugs/<int:amount>", methods=['GET'])
 def newest_bugs(amount):
-	return emit(query_newest_bugs(amount), True)
+	return emit(query("newest_bugs", amount), True)
 
 
 @bts.route("/get-versions/<package>/<dist>/<arch>", methods=['GET'])
--- deapi/services/bts.py
+++ deapi/services/bts.py
@@ -7,25 +7,8 @@ default_url = 'http://bugs.debian.org/cgi-bin/soap.cgi'
 ws = SOAPpy.SOAPProxy(default_url)
 
 
-def query_get_status(bugnumbers):
-	return getattr(ws, "get_status")(bugnumbers)
-
-
-def query_get_bugs(conditions):
-	return getattr(ws, "get_bugs")(conditions)
-
-
-def query_get_usertag(email):
-	return getattr(ws, "get_usertag")(email)
-
-
-def query_get_bug_log(bugnumber):
-	return getattr(ws, "get_bug_log")(bugnumber)
-
-
-def query_newest_bugs(amount):
-	return getattr(ws, "newest_bugs")(amount)
-
+def query(method, value):
+	return getattr(ws, method)(value)
 
 def query_get_versions(package, dist, arch):
 	return getattr(ws, "get_versions")(package, dist, arch)
